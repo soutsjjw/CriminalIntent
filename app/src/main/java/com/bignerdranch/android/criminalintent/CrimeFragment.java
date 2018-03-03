1742e7d4739a447ea524595d90eff60e7951e5a1
soutsjjw <souts.jjw@gmail.com>
2018-03-03 17:30:18 +0800

    15.5 挑戰練習：ShareCompat

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -111,11 +112,11 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT,
-                        getString(R.string.crime_report_subject));
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                        .setChooserTitle(getString(R.string.crime_report_subject))
+                        .setType("text/plain")
+                        .setText(getCrimeReport())
+                        .getIntent();
                 i = Intent.createChooser(i, getString(R.string.send_report));
                 startActivity(i);
             }
